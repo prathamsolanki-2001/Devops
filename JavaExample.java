271df2e629a96c1845d293fdd4cbeed00360019d
prathamsolanki-2001 <56110400+prathamsolanki-2001@users.noreply.github.com>
2021-10-30 15:38:42 +0530

    Update JavaExample.java

--- JavaExample.java
+++ JavaExample.java
@@ -9,10 +9,10 @@ public class JavaExample {
         int num1 = Integer.parseInt(num11);
         //System.out.print("Enter the marks of second subject: ");
         String num22 = System.getenv("b");
-        int num1 = Integer.parseInt(num11);
+        int num2 = Integer.parseInt(num11);
         //System.out.print("Enter the marks of third subject: ");
         String num33 = System.getenv("c");
-        int num1 = Integer.parseInt(num11);
+        int num3 = Integer.parseInt(num11);
         scan.close();
         System.out.print("The average of entered marks is:" + avr(num1, num2, num3) );
     }
