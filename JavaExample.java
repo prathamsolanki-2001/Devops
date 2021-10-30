e40ee8996c0a3a3d7195ac7cf541801fc3f8df53
prathamsolanki-2001 <56110400+prathamsolanki-2001@users.noreply.github.com>
2021-10-30 15:37:41 +0530

    Update JavaExample.java

--- JavaExample.java
+++ JavaExample.java
@@ -5,13 +5,13 @@ public class JavaExample {
     {
         Scanner scan = new Scanner(System.in);
         //System.out.print("Enter the marks of first subject: ");
-        String num11 = System.getenv("num1");
+        String num11 = System.getenv("a");
         int num1 = Integer.parseInt(num11);
         //System.out.print("Enter the marks of second subject: ");
-        String num22 = System.getenv("num2");
+        String num22 = System.getenv("b");
         int num1 = Integer.parseInt(num11);
         //System.out.print("Enter the marks of third subject: ");
-        String num33 = System.getenv("num3");
+        String num33 = System.getenv("c");
         int num1 = Integer.parseInt(num11);
         scan.close();
         System.out.print("The average of entered marks is:" + avr(num1, num2, num3) );
